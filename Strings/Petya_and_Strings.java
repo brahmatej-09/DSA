6e3245480b4de5b833b3b6efc2eaf34225d47fb9
brahmatej-09 <gayambrahmatejreddy@gmail.com>
2026-01-23 19:09:10 +0530

    Auto update: 23-01-2026 19:09:10.84

---
+++ Strings/Petya_and_Strings.java
@@ -0,0 +1,30 @@
+package Strings;
+import java.util.*;
+public class Petya_and_Strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        boolean flag = true;
+        for(int i=0;i<str1.length();i++){
+            int one = (int)str1.charAt(i);
+            int two = (int)str2.charAt(i);
+            if(one<two){
+                flag = false;
+                System.out.println(-1);
+                break;
+            }
+            else if(one>two){
+                flag = false;
+                System.out.println(1);
+                break;
+            }
+            else continue;
+        }
+        if(flag) System.out.println(0);
+        sc.close();
+    }
+    
+}
