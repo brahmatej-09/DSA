95794be4b5bbfdbf3ced43116c77ed6b970595e9
brahmatej-09 <gayambrahmatejreddy@gmail.com>
2026-02-11 14:11:19 +0530

    Auto update: 11-02-2026 14:11:19.55

---
+++ Trees/Basic.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val = val;
+    }
+}
+public class Basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+    
+}
