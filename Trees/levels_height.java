cbbb53c6956282c04e8848fc572619b36d8ed5f2
brahmatej-09 <gayambrahmatejreddy@gmail.com>
2026-02-17 18:22:36 +0530

    Auto update: 17-02-2026 18:22:36.70

--- Trees/levels_height.java
+++ Trees/levels_height.java
@@ -3,22 +3,19 @@ public class levels_height {
         TreeNode root = new TreeNode(1);
 
         root.left = new TreeNode(2);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(5);
-        root.left.right.left = new TreeNode(8);
-        root.left.left.left = new TreeNode(7);
         root.right = new TreeNode(3);
 
         root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(5);
+        root.left.left.left = new TreeNode(7);
 
-        root.right.right = new TreeNode(6);
+        root.right.left = new TreeNode(6);
+
         int l = level(root);
         System.out.println(l);
     }
     public static int level(TreeNode root) {
         if(root==null) return 0;
-        return(Math.max(1+level(root.left),1+level(root.right)));
+        return(Math.max(1+level(root.left),1+   level(root.right)));
     }
     
 }
